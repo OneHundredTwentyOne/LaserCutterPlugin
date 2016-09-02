1f090882f86584500ef4e26e952554e9283031b4
thepangman <azzapang40@yahoo.com>
2016-09-02 19:22:23 +1200

    Added test slider bar to tab

--- octoprint_lasercutter/__init__.py
+++ octoprint_lasercutter/__init__.py
@@ -4,31 +4,25 @@ from __future__ import absolute_import
 import octoprint.plugin
 
 class LaserCutterPlugin(octoprint.plugin.StartupPlugin,
-						octoprint.plugin.TemplatePlugin,
-						octoprint.plugin.SettingsPlugin,
-						octoprint.plugin.AssetPlugin):
-    def on_after_startup(self):
-        self._logger.info("Laser Cutter")
+                       octoprint.plugin.TemplatePlugin,
+                       octoprint.plugin.SettingsPlugin,
+                       octoprint.plugin.AssetPlugin):
+ def on_after_startup(self):
+     self._logger.info("Laser Cutter (more: %s)" % self._settings.get(["url"]))
 
-	def get_settings_defaults(self):
-		return dict(
-			test_setting="foo",
-			test_value=42,
-			sub=dict(
-				test_flag=true
-			)
-		)
+ def get_settings_defaults(self):
+     return dict(url="https://en.wikipedia.org/wiki/Hello_world")
 
-	def get_template_configs(self):
-		return[
-			dict(type="navbar", custom_bindings=False),
-			dict(type="settings", custom_bindings=False)
-		]
+ def get_template_configs(self):
+     return [
+         dict(type="navbar", custom_bindings=False),
+         dict(type="settings", custom_bindings=False)
+     ]
 
-	def get_assets(self):
-		return dict(
-			js=["js/lasercutter.js"]
-		)
+ def get_assets(self):
+     return dict(
+         js=["js/lasercutter.js"]
+     )
 
 __plugin_name__ = "Laser Cutter"
 __plugin_implementation__ = LaserCutterPlugin()
