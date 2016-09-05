b418a2787d2d966323c0ef46aec26d0215882443
thepangman <azzapang40@yahoo.com>
2016-09-05 23:38:11 +1200

    Aligned control panel correctly

--- octoprint_lasercutter/__init__.py
+++ octoprint_lasercutter/__init__.py
@@ -3,27 +3,31 @@ from __future__ import absolute_import
 
 import octoprint.plugin
 
+
 class LaserCutterPlugin(octoprint.plugin.StartupPlugin,
-                       octoprint.plugin.TemplatePlugin,
-                       octoprint.plugin.SettingsPlugin,
-                       octoprint.plugin.AssetPlugin):
- def on_after_startup(self):
-     self._logger.info("Laser Cutter (more: %s)" % self._settings.get(["url"]))
+                        octoprint.plugin.TemplatePlugin,
+                        octoprint.plugin.SettingsPlugin,
+                        octoprint.plugin.AssetPlugin):
+    def on_after_startup(self):
+        self._logger.info("Laser Cutter (more: %s)" % self._settings.get(["url"]))
 
- def get_settings_defaults(self):
-     return dict(url="https://en.wikipedia.org/wiki/Hello_world")
+    def get_settings_defaults(self):
+        return dict(url="https://en.wikipedia.org/wiki/Hello_world")
 
- def get_template_configs(self):
-     return [
-         dict(type="navbar", custom_bindings=False),
-         dict(type="settings", custom_bindings=False),
-         dict(type="tab", custom_bindings=False)
-     ]
+    def get_template_configs(self):
+        return [
+            dict(type="navbar", custom_bindings=False),
+            dict(type="settings", custom_bindings=False),
+            dict(type="tab", custom_bindings=False)
+        ]
 
- def get_assets(self):
-     return dict(
-         js=["js/lasercutter.js"]
-     )
+    def get_assets(self):
+        return dict(
+            js=["js/lasercutter.js"],
+            css=["css/lasercutter.css"],
+            less=["less/lasercutter.less"]
+        )
+
 
 __plugin_name__ = "Laser Cutter"
 __plugin_implementation__ = LaserCutterPlugin()
