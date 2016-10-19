b95c7de34373c800336ea929555ca09e1c3c6ecb
pangaaro <pangaaro@ecs.vuw.ac.nz>
2016-10-19 18:26:44 +1300

    Modifying .js file to match slicer plugin requirements

--- octoprint_lasercutter/static/js/lasercutter.js
+++ octoprint_lasercutter/static/js/lasercutter.js
@@ -2,7 +2,121 @@ $(function() {
     function HelloWorldViewModel(parameters) {
         var self = this;
 
-        self.settings = parameters[0];
+        self.loginState = parameters[0];
+        self.settingsViewModel = parameters[1];
+        self.slicingViewModel = parameters[2];
+
+        self.filename = ko.observable();
+
+        self.placeholderName = ko.observable();
+        self.placeholderDisplayName = ko.observable();
+        self.placeholderDesciption = ko.observable();
+
+        self.profileName = ko.observable();
+        self.profileDisplayName = ko.observable();
+        self.profileDescription = ko.observable();
+        self.profileAllowOverwrite = ko.observable();
+
+        self.uploadElement = $("settings-lasercutter-import");
+        self.uploadButton  = $("settings-lasercutter-import-start");
+
+        self.profiles = new ItemListHelper(
+            "plugin_lasercutter_profiles",
+            {
+                "id": function(a,b){
+                    if (a["key"].toLocaleLowerCase() < b["key"].toLocaleLowerCase()){
+                        return -1;
+                    }
+                    if (a["key"].toLocaleLowerCase() > b["key"].toLocaleLowerCase()){
+                        return -1;
+                    }
+                    return 0;
+                },
+                "name": function(a,b){
+                    var name1 = a.name();
+                    if(name1 === undefined){
+                        name1 = "";
+                    }
+                    var name2 = b.name();
+                    if(name2 === undefined){
+                        name2 = "";
+                    }
+                    if(name1.toLocaleLowerCase() < name2.toLocaleLowerCase()){
+                        return -1;
+                    }
+                    if(name1.toLocaleLowerCase() < name2.toLocaleLowerCase()){
+                        return -1;
+                    }
+                    return 0;
+                }
+            }, {}, "id", [], [], 5
+        );
+
+        self.uploadElement.fileupload({
+            dataType: "json",
+            maxNumberOfFiles: 1,
+            autoUpload: false,
+            add: function(e, data) {
+                if (data.files.length == 0) {
+                    return false;
+                }
+                self.fileName(data.files[0].name);
+                var name = self.fileName().substr(0, self.fileName().lastIndexOf("."));
+                self.placeholderName(self._sanitize(name).toLowerCase());
+                self.placeholderDisplayName(name);
+                self.placeholderDescription("Imported from " + self.fileName() + " on " + formatDate(new Date().getTime() / 1000));
+                self.uploadButton.unbind("click");
+                self.uploadButton.on("click", function() {
+                    var form = {
+                        allowOverwrite: self.profileAllowOverwrite()
+                    };
+
+                    if (self.profileName() !== undefined) {
+                        form["name"] = self.profileName();
+                    }
+                    if (self.profileDisplayName() !== undefined) {
+                        form["displayName"] = self.profileDisplayName();
+                    }
+                    if (self.profileDescription() !== undefined) {
+                        form["description"] = self.profileDescription();
+                    }
+                    data.formData = form;
+                    data.submit();
+                });
+            },
+            done: function(e, data) {
+                self.fileName(undefined);
+                self.placeholderName(undefined);
+                self.placeholderDisplayName(undefined);
+                self.placeholderDescription(undefined);
+                self.profileName(undefined);
+                self.profileDisplayName(undefined);
+                self.profileDescription(undefined);
+                self.profileAllowOverwrite(true);
+                $("#settings_plugin_lasercutter_import").modal("hide");
+                self.requestData();
+                self.slicingViewModel.requestData();
+            }
+        });
+
+        self.removeProfile = function(data) {
+            if (!data.resource) {
+                return;
+            }
+
+            self.profiles.removeItem(function(item) {
+                return (item.key == data.key);
+            });
+
+            $.ajax({
+                url: data.resource(),
+                type: "DELETE",
+                success: function() {
+                    self.requestData();
+                    self.slicingViewModel.requestData();
+                }
+            });
+        };
 
         // this will hold the URL currently displayed by the iframe
         self.currentUrl = ko.observable();
