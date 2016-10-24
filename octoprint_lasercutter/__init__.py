ac3297ed6cabb94c1800e6256e29770dbf19f403
thepangman <azzapang40@yahoo.com>
2016-10-24 18:41:13 +1300

    Added profile saving and importing

--- octoprint_lasercutter/__init__.py
+++ octoprint_lasercutter/__init__.py
@@ -27,6 +27,7 @@ class LaserCutterPlugin(octoprint.plugin.StartupPlugin,
 
 	def __init__(self):
 		print "Initialising OctoPrint LaserCutter plugin"
+		self._logger = logging.getLogger("octoprint.plugins.lasercutter")
 		self._slicing_commands = dict()
 		self._cancelled_jobs = []
 
@@ -48,11 +49,79 @@ class LaserCutterPlugin(octoprint.plugin.StartupPlugin,
 			destination_extensions = ["gco", "gcode", "g"]
 		)
 
+	@octoprint.plugin.BlueprintPlugin.route("/import", methods=["POST"])
+	def import_laser_profile(self):
+		import tempfile
+
+		from octoprint.server import slicingManager
+
+		input_name = "file"
+		input_upload_name = input_name + "." + self._settings.global_get(["server", "uploads", "nameSuffix"])
+		input_upload_path = input_name + "." + self._settings.global_get(["server", "uploads", "pathSuffix"])
+
+		if input_upload_name in flask.request.values and input_upload_path in flask.request.values:
+			filename = flask.request.values[input_upload_name]
+			try:
+				profile_dict = Profile.from_cura_ini(flask.request.values[input_upload_path])
+			except Exception as e:
+				self._logger.exception("Error while converting the imported profile")
+				return flask.make_response("No file included", 400)
+
+		if profile_dict is None:
+			self._logger.warn("Could not convert profile, aborting")
+			return flask.make_response("Could not convert profile", 400)
+
+		name, _ = os.path.splitext(filename)
+
+		profile_name = name
+		profile_display_name = name
+		profile_description = "Laser profile"
+		profile_allow_overwrite = False
+
+		if "name" in flask.request.values:
+			profile_name = flask.request.values["name"]
+		if "displayName" in flask.request.values:
+			profile_display_name = flask.request.values["displayName"]
+		if "description" in flask.request.values:
+			profile_description = flask.request.values["description"]
+		if "allowOverwrite" in flask.request.values:
+			from octoprint.server.api import valid_boolean_trues
+			profile_allow_overwrite = flask.request.values["allowOverwrite"] in valid_boolean_trues
+
+		try:
+			slicingManager.save_profile("lasercutter", profile_name, profile_dict,
+										allow_overwrite = profile_allow_overwrite, display_name = profile_display_name,
+										description = profile_description)
+		except octoprint.slicing.ProfileAlreadyExists:
+			self._logger.warn("Profile already exists")
+			return flask.make_response("Profile already exists")
+
+		result = dict(
+			resource = flask.url_for("api.slicingGetSlicerProfile", slicer = "lasercutter", name = profile_name,
+									 external = True),
+			displayName = profile_display_name,
+			description = profile_description
+		)
+		r = flask.make_response(flask.jsonify(result), 201)
+		r.headers["Location"] = result["resource"]
+		return r
+
+
 	def get_slicer_default_profile(self):
 		path = self._settings.get(["default_profile"])
 		if not path:
 			path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "profiles", "default.profile.yaml")
 		return self.get_slicer_profile(path)
+
+	def save_slicer_profile(self, path, profile, allow_overwrite = True, overrides = None):
+		new_profile = Profile.merge_profile(profile.data, overrides = overrides)
+		if profile.display_name is not None:
+			new_profile["_display_name"] = profile.display_name
+		if profile.description is not None:
+			new_profile["_description"] = profile.description
+
+		self._save_profile(path, new_profile, allow_overwrite = allow_overwrite)
+
 
 	def get_slicer_profile(self, path):
 		profile_dict = self._load_profile(path)
@@ -74,6 +143,8 @@ class LaserCutterPlugin(octoprint.plugin.StartupPlugin,
 	def is_slicer_configured(self):
 		print "Set to true so it should always be enabled"
 		return True
+
+
 
 	@property
 	def slicing_enabled(self):
--- octoprint_lasercutter/static/js/lasercutter.js
+++ octoprint_lasercutter/static/js/lasercutter.js
@@ -2,28 +2,181 @@ $(function() {
     function LaserCutterViewModel(parameters) {
         var self = this;
 
-        self.settings = parameters[0];
+        self.loginState = parameters[0];
+        self.settingsViewModel = parameters[1];
+        self.slicingViewModel = parameters[2];
 
-        // this will hold the URL currently displayed by the iframe
-        self.currentUrl = ko.observable();
+        self.filename = ko.observable();
 
-        // this will hold the URL entered in the text field
-        self.newUrl = ko.observable();
+        self.placeholderName = ko.observable();
+        self.placeholderDisplayName = ko.observable();
+        self.placeholderDesciption = ko.observable();
 
-        // this will be called when the user clicks the "Go" button and set the iframe's URL to
-        // the entered URL
-        self.goToUrl = function() {
-            self.currentUrl(self.newUrl());
+        self.profileName = ko.observable();
+        self.profileDisplayName = ko.observable();
+        self.profileDescription = ko.observable();
+        self.profileAllowOverwrite = ko.observable();
+
+        self.uploadElement = $("settings-lasercutter-import");
+        self.uploadButton  = $("settings-lasercutter-import-start");
+
+        self.profiles = new ItemListHelper(
+            "plugin_lasercutter_profiles",
+            {
+                "id": function(a,b){
+                    if (a["key"].toLocaleLowerCase() < b["key"].toLocaleLowerCase()){
+                        return -1;
+                    }
+                    if (a["key"].toLocaleLowerCase() > b["key"].toLocaleLowerCase()){
+                        return -1;
+                    }
+                    return 0;
+                },
+                "name": function(a,b){
+                    var name1 = a.name();
+                    if(name1 === undefined){
+                        name1 = "";
+                    }
+                    var name2 = b.name();
+                    if(name2 === undefined){
+                        name2 = "";
+                    }
+                    if(name1.toLocaleLowerCase() < name2.toLocaleLowerCase()){
+                        return -1;
+                    }
+                    if(name1.toLocaleLowerCase() < name2.toLocaleLowerCase()){
+                        return -1;
+                    }
+                    return 0;
+                }
+            }, {}, "id", [], [], 5
+        );
+
+        self.uploadElement.fileupload({
+            dataType: "json",
+            maxNumberOfFiles: 1,
+            autoUpload: false,
+            add: function(e, data) {
+                if (data.files.length == 0) {
+                    return false;
+                }
+                self.fileName(data.files[0].name);
+                var name = self.fileName().substr(0, self.fileName().lastIndexOf("."));
+                self.placeholderName(self._sanitize(name).toLowerCase());
+                self.placeholderDisplayName(name);
+                self.placeholderDescription("Imported from " + self.fileName() + " on " + formatDate(new Date().getTime() / 1000));
+                self.uploadButton.unbind("click");
+                self.uploadButton.on("click", function() {
+                    var form = {
+                        allowOverwrite: self.profileAllowOverwrite()
+                    };
+
+                    if (self.profileName() !== undefined) {
+                        form["name"] = self.profileName();
+                    }
+                    if (self.profileDisplayName() !== undefined) {
+                        form["displayName"] = self.profileDisplayName();
+                    }
+                    if (self.profileDescription() !== undefined) {
+                        form["description"] = self.profileDescription();
+                    }
+                    data.formData = form;
+                    data.submit();
+                });
+            },
+            done: function(e, data) {
+                self.fileName(undefined);
+                self.placeholderName(undefined);
+                self.placeholderDisplayName(undefined);
+                self.placeholderDescription(undefined);
+                self.profileName(undefined);
+                self.profileDisplayName(undefined);
+                self.profileDescription(undefined);
+                self.profileAllowOverwrite(true);
+                $("#settings_plugin_lasercutter_import").modal("hide");
+                self.requestData();
+                self.slicingViewModel.requestData();
+            }
+        });
+
+        self.removeProfile = function(data) {
+            if (!data.resource) {
+                return;
+            }
+
+            self.profiles.removeItem(function(item) {
+                return (item.key == data.key);
+            });
+
+            $.ajax({
+                url: data.resource(),
+                type: "DELETE",
+                success: function() {
+                    self.requestData();
+                    self.slicingViewModel.requestData();
+                }
+            });
         };
 
-        // This will get called before the HelloWorldViewModel gets bound to the DOM, but after its
-        // dependencies have already been initialized. It is especially guaranteed that this method
-        // gets called _after_ the settings have been retrieved from the OctoPrint backend and thus
-        // the SettingsViewModel been properly populated.
-        self.onBeforeBinding = function() {
-            self.newUrl(self.settings.settings.plugins.helloworld.url());
-            self.goToUrl();
-        }
+        self.makeProfileDefault = function(data){
+            if(!data.resources){
+                return;
+            }
+
+            _.each(self.profiles.items(),function(item){
+               item.isdefault(false);
+            });
+
+            var item = self.profiles.getItem(function(item){
+                return item.key == data.key;
+            });
+            if(item !== undefined){
+                item.isdefault(true);
+            }
+
+            $.ajax({
+                url: data.resource(),
+                type: "PATCH",
+                dataType: "json",
+                data: JSON.stringify({default: true}),
+                contentType: "application/json; charset=UTF-8",
+                success: function() {
+                    self.requestData();
+                }
+            });
+        };
+
+        self.showImportProfileDialog = function(){
+          $("#settings_plugin_lasercutter_import").modal("show");
+        };
+
+        self.requestData = function(){
+            $.ajax({
+                url: API_BASEURL + "slicing/lasercutter/profiles",
+                type: "GET",
+                dataType: "json",
+                success: self.fromResponse
+            });
+        };
+
+        self.fromResponse = function(data){
+            var profiles = [];
+            _.each(_.keys(data), function(key){
+                profiles.push({
+                    key: key,
+                    name: ko.observable(data[key].displayName),
+                    description: ko.observable(data[key].description),
+                    isdefault: ko.observable(data[key].default),
+                    resource: ko.observable(data[key].resource)
+                })
+            });
+            self.profile.updateItems(profiles);
+        };
+
+        self.onBeforeBinding = function(){
+            self.settings = self.settingsViewModel.settings;
+            self.requestData();
+        };
     }
 
     // This is how our plugin registers itself with the application, by adding some configuration
@@ -35,9 +188,7 @@ $(function() {
         // This is a list of dependencies to inject into the plugin, the order which you request
         // here is the order in which the dependencies will be injected into your view model upon
         // instantiation via the parameters argument
-        ["settingsViewModel"],
-
-        // Finally, this is the list of selectors for all elements we want this view model to be bound to.
-        ["#tab_plugin_lasercutter"]
+        ["loginStateViewModel, settingsViewModel, slicingViewModel"],
+        "#settings_plugin_lasercutter"
     ]);
 });
