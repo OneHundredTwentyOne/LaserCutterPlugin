3db6bf8a01d83824b1614eaf15879a34e521dfe3
thepangman <azzapang40@yahoo.com>
2016-10-27 23:15:51 +1300

    Edited plugin

--- octoprint_lasercutter/__init__.py
+++ octoprint_lasercutter/__init__.py
@@ -120,7 +120,7 @@ class LaserCutterPlugin(octoprint.plugin.StartupPlugin,
 		print "DESC: "  + description
 		properties = self.get_slicer_properties()
 		print "PROPERTIES" + str(properties)
-		return [octoprint.slicing.SlicingProfile(properties["type"], "DEFAULT", profile_dict, display_name = display_name, description = description)]
+		return octoprint.slicing.SlicingProfile(properties["type"], "DEFAULT", profile_dict, display_name = display_name, description = description)
 
 
 	def do_slice(self, model_path, printer_profile, machinecode_path = None, profile_path = None,
